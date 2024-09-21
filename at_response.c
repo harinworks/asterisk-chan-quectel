db52fdfea5bc23ede9feeb4672f77363ab5df861
IchthysMaranatha <94102122+IchthysMaranatha@users.noreply.github.com>
2024-09-21 14:07:41 +0530

    Update at_response.c

--- at_response.c
+++ at_response.c
@@ -125,6 +125,7 @@ static int at_response_dtmfdet (struct pvt * pvt, const char* str)
         sscanf (str, "+QTONEDET: %d", &dtmfdigit);
 		ast_log (LOG_ERROR, "local dtmf accessed '%d'\n", dtmfdigit);
         dtmf_alloc(pvt, dtmfdigit);
+	return 0;
 }
         
 
